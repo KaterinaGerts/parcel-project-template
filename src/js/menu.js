99a3838d86c8c655249f6438aad5809a6af6ddca
miLLlog <milllog@mail.ru>
2021-03-17 10:48:06 +0300

    исправил меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,15 @@
-$('.button-menu').click(function() {
+$('.button-menu').click(function () {
   $('.mobile-menu').toggleClass('is-open');
   $('.button-menu').toggleClass('is-open');
   $('body').toggleClass('is-open');
 });
-    
-$(document).mouseup(function(e) { // событие клика по веб-документ
-  var div = $(".mobile-menu"); // тут указываем class элемента
-  var toggle = $('.button-menu');
-  if (!div.is(e.target)
-  &&
-    !toggle.is(e.target)) { // если клик был не по нашему блоку 
+
+$(document).mouseup(function (e) { // событие клика по веб-документ
+  var menu = $(".mobile-menu .link"); // тут указываем class элемента
+  var btn = $('.button-menu');
+  if (!btn.is(e.target)
+    &&
+    menu.is(e.target)) { // если клик был не по нашему блоку 
     $(".button-menu").removeClass('is-open');
     $(".mobile-menu").removeClass('is-open');
     $("body").removeClass('is-open');
